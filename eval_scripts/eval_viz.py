daa58449dd653c14125781ae0824a80fa4bfcb91
evaprakash <eprakash@anomaly-jenny-west.c.hai-gcp-neuroimages.internal>
2023-04-26 22:32:54 +0000

    Updated eval

--- eval_scripts/eval_viz.py
+++ eval_scripts/eval_viz.py
@@ -9,28 +9,27 @@ import cv2
 DATASET = "/home/eprakash/shanghaitech/testing/small_obj_test_list.txt"
 IDX = 1549
 
-device = 'cuda:0'
+device = 'cuda:4'
 conf = video_64_autoenc()
 model = LitModel(conf)
 
-state = torch.load(f'/home/evaprakash/video-diffae/checkpoints/resnet18_normfix/last.ckpt', map_location=device)
+state = torch.load(f'/home/eprakash/temp/video-diffae-git/video-diffae/checkpoints/resnet50/last.ckpt', map_location=device)
 model.load_state_dict(state['state_dict'], strict=False)
-model.model.eval()
-model.model.to(device)
+model.ema_model.eval()
+model.ema_model.to(device)
 
 data = ShanghaiTechDataset(path=DATASET, need_eval=True, image_size=conf.img_size, cf_stride=False)
 frame_batch = data[IDX]['img'][None]
-frame_batch_ori = data[IDX]['img_resnet'][None]
 
 print("Encoding...")
-cond = model.encode(frame_batch_ori.to(device))
+cond = model.encode(frame_batch.to(device))
 
 #Use random semantic subcode
 #seed = np.random.randint(0, 1000000)
 #torch.manual_seed(seed)
 #cond = torch.randn(1, 512, device=device)
 
-xT = model.encode_stochastic(x=frame_batch.to(device), cond=cond, T=2)
+xT = model.encode_stochastic(x=frame_batch.to(device), cond=cond, T=None)
 
 #Use random stochastic encoding
 #xT = torch.randn(1, 3, 9, conf.img_size, conf.img_size, device=device)
@@ -41,6 +40,7 @@ pred = model.render(noise=xT, cond=cond, T=None)
 print("Plotting...")
 F = 9
 fig, ax = plt.subplots(1, F, figsize=(F * 5, 5))
+frame_batch_ori = (frame_batch + 1)/2
 frame_batch_ori = frame_batch_ori.permute(0, 2, 1, 3, 4)
 
 for i in range(F):
