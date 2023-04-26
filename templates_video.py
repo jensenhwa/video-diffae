6aafea402aad97c5a08c5fb7535dc9c909d0b5af
Jensen Hwa <30474284+jensenhwa@users.noreply.github.com>
2023-04-26 19:36:27 +0000

    Update config with sample_size to avoid eval error

--- templates_video.py
+++ templates_video.py
@@ -1,4 +1,4 @@
-from templates import ffhq64_autoenc
+from templates import ffhq64_autoenc, ffhq128_autoenc_130M
 
 
 def shanghai_64_autoenc():
@@ -11,18 +11,29 @@ def shanghai_64_autoenc():
     conf.name = 'shanghai_autoenc'
     return conf
 
+
 def video_64_autoenc():
     conf = ffhq64_autoenc()
-    #conf.net_ch_mult = (1, 2, 4, 4)
-    conf.net_ch_mult = (1,1)
+    # conf.net_ch_mult = (1, 2, 4, 4)
+    conf.net_ch_mult = (1, 1)
     conf.net_enc_channel_mult = (1, 2, 4, 8, 8)
     conf.data_name = 'video'
     conf.eval_num_images = 5_000
     conf.eval_every_samples = 10_000_000
     conf.eval_ema_every_samples = 10_000_000
     conf.total_samples = 1_000_000
-    # conf.batch_size = 4
-    conf.net_ch = 8
+    conf.batch_size = 16
+    conf.sample_size = 16
+    conf.net_ch = 4
+    conf.net_beatgans_embed_channels = 512  # size of semantic encoding
+    conf.name = 'temp'  # 'video_st_flow+gs'
+    return conf
+
+
+def video_128_autoenc():
+    conf = ffhq128_autoenc_130M()
+    conf.data_name = 'video'
     conf.net_beatgans_embed_channels = 512
-    conf.name = 'temp'#'video_st_flow+gs'
+    conf.batch_size = 16
+    conf.sample_size = 16
     return conf
